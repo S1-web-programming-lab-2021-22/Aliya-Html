00a87121b813bcc04d65a501d38d99b3a2bf0616
aliya175 <94426677+aliya175@users.noreply.github.com>
2022-03-02 09:37:47 +0530

    Add files via upload

---
+++ largest2.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+<title>Largest of three numbers</title>
+</head>
+<body>
+<?php
+$a=10;
+$b=5;
+$c=14;
+if($a>$b && $a>$c)
+{
+echo"a is large";
+}
+else if($b>$a && $b>$c)
+{
+echo"b is large";
+}
+else
+{
+echo"c is large";
+}
+?>
+</body>
+</html>
